6178e6b34fbbba28124acfc17bb541b61d8d816c
anthrod <anth.jrod@gmail.com>
2019-08-04 21:29:57 -0700

    sec-edgar is ready for use

--- hunters/financials/sec-edgar.py
+++ hunters/financials/sec-edgar.py
@@ -10,15 +10,15 @@ wikipedia_snp500_html_url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_com
 cache_varname = "GREENLIGHT_CACHE_PATH"
 snp500_symbols_filename = "snp500_symbols.txt"
 market_symbols_filenames = ["nasdaq.csv", "nyse.csv", "amex.csv"]
-commonstocks_10k_cachedir_name   = os.path.join("stocks","10K")
+commonstocks_10k_cachedir_name = os.path.join("stocks","10K")
 edgar_cikmap_filename = "cik_ticker.csv"
+edgar_addresses_dirname = "edgar-addresses"
 
 cik_ticker_url = "http://rankandfiled.com/static/export/cik_ticker.csv"
 
-msKRstub  = "http://financials.morningstar.com/ajax/exportKR2CSV.html?t="
-msFinSeg1 = "http://financials.morningstar.com/ajax/ReportProcess4CSV.html?t="
-msFinSeg2 = "&reportType="
-msFinSeg3 = "&period=12&dataType=A&order=asc&denominatorView=decimal&columnYear=5&number=3"
+edgar_url_stub = "https://www.sec.gov/Archives/"
+
+quarter_edgar_tsv_identifiers = ["QTR1", "QTR2", "QTR3", "QTR4"]
 
 def getfile(url, filepath):
   if os.path.exists(filepath):
@@ -50,7 +50,6 @@ def download_cik_ticker(url=cik_ticker_url, filepath=os.path.join(os.environ[cac
   with open(filepath, "w+") as outputfile:
     with urllib.request.urlopen(url) as response:
       lines = response.read().decode('utf-8').split('\n')
-      print(lines)
       for line in lines:
         outputfile.write( line + "\n" )
   if os.stat(filepath).st_size==0:
@@ -109,29 +108,74 @@ def fetch_ticker_cik_map(filepath=os.path.join(os.environ[cache_varname], edgar_
       cik_map[entries[1]] = entries[0]
   return cik_map
 
+def handleMissingEdgarIndex(edgar_addresses_dirpath):
+  print("Downloading EDGAR address tables to \"" + edgar_addresses_dirpath + "\"")
+  if not os.path.exists(edgar_addresses_dirpath): 
+    os.mkdir(edgar_addresses_dirpath)
+  import edgar
+  edgar.download_index(edgar_addresses_dirpath, 1993) 
+
+def download_10k(url, dest):
+  with open(dest, "w+") as outputfile:
+    with urllib.request.urlopen(url) as response:
+      lines = response.read().decode('utf-8').split('\n')
+      for line in lines:
+        outputfile.write( line + "\n" )
+  if os.stat(dest).st_size==0:
+    raise IOError("Could not download from " + url)    
+
 def pull_financials(cache_path, args):
   symbols = fetchsymbols(cache_path, args)
-  cikmap = fetch_ticker_cik_map()
   data_dir   = os.path.join(cache_path, commonstocks_10k_cachedir_name)
+  edgar_cikmap_filepath = os.path.join(os.environ[cache_varname], edgar_cikmap_filename)
+  if os.path.exists(edgar_cikmap_filepath) and args.purgeOldData:
+    os.remove(edgar_cikmap_filepath)
+  if not os.path.exists(edgar_cikmap_filepath):
+    print("Downloading CIK->symbol map file to \"" + edgar_cikmap_filepath + "\"")
+    download_cik_ticker() 
   if os.path.isdir(data_dir) and args.purgeOldData: shutil.rmtree(data_dir)
   if not os.path.isdir(data_dir): os.mkdir(data_dir)
+  edgar_addresses_dirpath = os.path.join(os.environ[cache_varname], edgar_addresses_dirname)
+  if os.path.isdir(edgar_addresses_dirpath) and args.purgeOldData:
+    shutil.rmtree(edgar_addresses_dirpath)
+  if not os.path.isdir(edgar_addresses_dirpath):
+    handleMissingEdgarIndex(edgar_addresses_dirpath)
+  edgar_address_files = os.listdir(edgar_addresses_dirpath)
+  if len(edgar_address_files)==0:
+    handleMissingEdgarIndex(edgar_addresses_dirpath)    
+  cikmap = fetch_ticker_cik_map()
   for symbol in symbols:
+    if not symbol in cikmap:
+      print("Warning: Could not find symbol \"" + symbol + "\" in the CIK->symbol map. Won't be able to fetch data")
+      continue
+    cik = cikmap[symbol]
     symbol_dir = os.path.join(data_dir, symbol)
     if not os.path.isdir(symbol_dir):
       os.mkdir(symbol_dir)
-    print("Pulling financial data for " + symbol)
-          
+    print("Pulling 10-K reports for " + symbol)
+    for addressfilename in edgar_address_files:
+      year = addressfilename[0:4]
+      address_filepath = os.path.join(edgar_addresses_dirpath, addressfilename)
+      with open(address_filepath, "r") as addressfile:
+        lines = addressfile.read().split("\n")
+        for line in lines:
+          if cik in line[0:7] and "|10-K|" in line:
+            url = edgar_url_stub + line.split("|")[5]
+            datafilename = os.path.join(os.environ[cache_varname], commonstocks_10k_cachedir_name)
+            datafilename = os.path.join(datafilename, symbol)
+            datafilename = os.path.join(datafilename, year + ".txt")
+            #print(symbol + ", " + year + ", " + url + " =-> " + datafilename ) 
+            if os.path.exists(datafilename):
+              print(datafilename + " exists")
+            else:
+              print("Downloading " + datafilename + " from " + url)
+              download_10k(url, datafilename)
 
 def verify_cache():
   if not cache_varname in os.environ or not os.path.isdir(os.environ[cache_varname]):
     print("Error: This program saves data to the directory in the user's "\
           + cache_varname + " directory. Please define this variable and try again")
     sys.exit(1)
-  edgar_cikmap_filepath = os.path.join(os.environ[cache_varname], edgar_cikmap_filename)
-  if not os.path.exists(edgar_cikmap_filepath):
-    print("Warning: Could not find CIK->symbol map file at " + edgar_cikmap_filepath)
-    print("Attempting to download...")
-    download_cik_ticker() 
 
 if __name__ == '__main__':
   """If running this script standalone, fetch S&P500 symbols and write the symbol file to
